d662acafa0b5120c7057d1d30d9bc700355a9531
LevOrlov <ghatuin12@gmail.com>
2018-03-11 19:43:37 +0300

    Lesson3_Exercise_4_String

---
+++ src/MainString.java
@@ -0,0 +1,8 @@
+public class MainString {
+    public static void main(String[] args) {
+        String str = "a";
+        System.out.print(str);
+        str = "b";
+        System.out.print(str);
+    }
+}
