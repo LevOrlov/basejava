6f792d952fa77c334726ed17956d6c14bff2d9c3
LevOrlov <ghatuin12@gmail.com>
2018-05-15 21:27:51 +0300

    Lesson15_servlets added output to web, from DB

--- src/web/Web.java
+++ src/web/Web.java
@@ -1,11 +1,24 @@
 package web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.SqlStorage;
+import ru.javawebinar.basejava.storage.serializer.JsonStreamSerializer;
+import ru.javawebinar.basejava.util.JsonParser;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import static ru.javawebinar.basejava.model.ContactType.PHONE;
 
 
 /**
@@ -13,45 +26,47 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public final class Web extends HttpServlet {
+//    protected static final File STORAGE_DIR = Config.get().getStorageDir();
+
+
+
+    //private static final SqlStorage sql;
+
+
+
+         //   sql.save( new Resume(UUID_1, "Name1"));
 
 
     /**
      * Respond to a GET request for the content produced by
      * this servlet.
      *
-     * @param request The servlet request we are processing
+     * @param request  The servlet request we are processing
      * @param response The servlet response we are producing
-     *
-     * @exception IOException if an input/output error occurs
-     * @exception ServletException if a servlet error occurs
+     * @throws IOException      if an input/output error occurs
+     * @throws ServletException if a servlet error occurs
      */
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
             throws IOException, ServletException {
-
-        response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<head>");
-        writer.println("<title>Sample Application Servlet Page</title>");
-        writer.println("</head>");
-        writer.println("<body bgcolor=white>");
 
-        writer.println("<table border=\"0\" cellpadding=\"10\">");
-        writer.println("<tr>");
-        writer.println("<td>");
-        writer.println("<img src=\"images/springsource.png\">");
-        writer.println("</td>");
-        writer.println("<td>");
-        writer.println("<h1>Sample Application Servlet</h1>");
-        writer.println("</td>");
-        writer.println("</tr>");
-        writer.println("</table>");
 
-        writer.println("This is the output of a servlet that is part of");
-        writer.println("the Hello, World application.");
+          try{
+              Class.forName("org.postgresql.Driver");
+        SqlStorage sql = new SqlStorage("jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "qazxswedc123");
+              writer.println("Got Connection");
+          writer.println(sql.get("uuid1"));
+          sql.update(new Resume("uuid1","Fuck You"));
+              writer.println(sql.get("uuid1"));
+          }
+                catch (Exception e){
+                    writer.println("DB not connection");
+                }
 
-        writer.println("</body>");
-        writer.println("</html>");
+
+
     }
 } 
